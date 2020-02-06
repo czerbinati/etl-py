064c2fcce22e540e1a131f14369c0898177f2dd1
christian <christian.zerbinati@abstract.it>
2020-02-06 14:21:49 +0100

    Adding file reading

--- modules/sftp_conn/connect.py
+++ modules/sftp_conn/connect.py
@@ -1,15 +1,16 @@
 import pysftp
+import ..fileread/inputfile
 
-hostname = 'sftp'
-user = 'username'
-pwd = 'password'
+def setup_conn(hostname, user, pwd):
 
-with pysftp.Connection(host=hostname, username=user, password=pwd) as sftp:
-    print("Connection succesfully stablished...REMOTE directory --> "+sftp.pwd)
+    with pysftp.Connection(host=hostname, username=user, password=pwd) as sftp:
+        print("Connection succesfully stablished...REMOTE directory --> "+sftp.pwd)
 
-    sftp.cwd('/AFS1/')
-    directory_path = sftp.listdir_attr()
+        sftp.cwd('/AFS1/')
+        directory_path = sftp.listdir_attr()
 
-    for attr in directory_path:
-        print(attr.filename, attr)
+        for attr in directory_path:
+            print(attr.filename, attr)
 
+
+setup_conn('reconciliation2.milleri.it', 'sftprecon', 'R3c0nc1l1@t10n$2019')
